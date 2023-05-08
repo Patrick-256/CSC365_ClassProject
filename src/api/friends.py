d24e7e2d1de829ffeae1e2b3fcf6e545bedb4ec7
Patrick-256 <321buildablock123@gmail.com>
2023-05-08 14:50:17 -0700

    adjusted friends api for new table format

--- src/api/friends.py
+++ src/api/friends.py
@@ -18,31 +18,18 @@ class Friend:
 def add_friend(new_friend: Friend):
     """adds a friend to the users friends
     """
-    #figure out friend id to assign
-    lastFriendId_query = sqlalchemy.select(db.friends.c.friendship_id).order_by(sqlalchemy.desc(db.friends.c.friendship_id))
-
-    new_user_id = None
-    with db.engine.connect() as conn:
-        lastUserId_result = conn.execute(lastFriendId_query)      
-        lastUserId = lastUserId_result.fetchone()
-        if lastUserId is None:
-            new_user_id = 0
-        else:
-            new_user_id = lastUserId.friendship_id + 1
-
     #Should probably check the user_id's to make sure they exist and they
     #are not already friends before adding the friendship between them
 
     insert_statement = """
-    INSERT INTO friends (friendship_id,user1_id, user2_id)
-    VALUES ({}, {}, {})
-    """.format(new_user_id,new_friend.user1_id,new_friend.user2_id)
+    INSERT INTO friends (user1_id, user2_id)
+    VALUES ({}, {})
+    """.format(new_friend.user1_id,new_friend.user2_id)
 
     with db.engine.begin() as conn:
-        addFriendResult = conn.execute(sqlalchemy.text(insert_statement))
+        conn.execute(sqlalchemy.text(insert_statement))
 
-        print(addFriendResult)
-    return {"Added friend to the database!"}
+    return {"Added friendship to the database!"}
 
 
 @router.get("/friends/",tags=["users"])
@@ -62,7 +49,6 @@ def list_users():
         res_json = []
         for row in result:
             res_json.append({
-                "friendship_id":row.friendship_id,
                 "user1_id":row.user1_id,
                 "user2_id":row.user2_id,
                 "created_at":row.created_at,
