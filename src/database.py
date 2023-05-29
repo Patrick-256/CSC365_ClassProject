386a601099cfffb34443f3b9fe47661508a634ce
Patrick-256 <321buildablock123@gmail.com>
2023-05-29 13:03:47 -0700

    commiting DB file to use shared DB link

--- src/database.py
+++ src/database.py
@@ -10,8 +10,8 @@ def database_connection_url():
     DB_SERVER: str = os.environ.get("POSTGRES_SERVER")
     DB_PORT: str = os.environ.get("POSTGRES_PORT")
     DB_NAME: str = os.environ.get("POSTGRES_DB")
-    return f"postgresql://postgres:postgres@localhost:54322/postgres" # use for local DB
-    #return f"postgresql://{DB_USER}:{DB_PASSWD}@{DB_SERVER}:{DB_PORT}/{DB_NAME}" #use for shared DB
+    #return f"postgresql://postgres:postgres@localhost:54322/postgres" # use for local DB
+    return f"postgresql://{DB_USER}:{DB_PASSWD}@{DB_SERVER}:{DB_PORT}/{DB_NAME}" #use for shared DB
 
 # Create a new DB engine based on our connection string
 engine = sqlalchemy.create_engine(database_connection_url())
